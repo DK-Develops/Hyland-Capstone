29d3f95991456b559861a4bd914f50c172d90f54
Darsh <darshkanuganti@gmail.com>
2026-03-17 19:22:04 -0400

    fix api

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const url = 'https://api.weatherstack.com/current?access_key={PASTE_YOUR_API_KEY_HERE}&query=New Delhi';
+const options = {
+	method: 'GET'
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
 
